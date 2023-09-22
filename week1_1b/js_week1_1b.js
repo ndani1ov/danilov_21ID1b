6a206e34de4b3b1043018c85de612c3fc3709f80
ndani1ov <132368604+ndani1ov@users.noreply.github.com>
2023-09-22 10:21:33 +0300

    fixed

--- week1_1b/js_week1_1b.js
+++ week1_1b/js_week1_1b.js
@@ -1,4 +1,4 @@
 function displayValue() {
-    var name = document.getElementById("name").value;
-    document.getElementById("output").textContent = name;
+    var nameuser = document.getElementById("name").value;
+    document.getElementById("output").textContent = "Привет, " + nameuser + "!";
 }
--- week1_1b/week1_1b/js_week1_1b.js
+++
@@ -1,4 +0,0 @@
-function displayValue() {
-    var nameuser = document.getElementById("name").value;
-    document.getElementById("output").textContent = "Привет, " + nameuser + "!";
-}

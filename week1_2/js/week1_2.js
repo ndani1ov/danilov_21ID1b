85eb052ab4685a9c0720cc0a50a31183f884142c
ndani1ov <132368604+ndani1ov@users.noreply.github.com>
2023-09-24 11:37:53 +0300

    добавлено задание 2 первой недели

--- week1_2/js/week1_2.js
+++ week1_2/js/week1_2.js
@@ -1,9 +1,17 @@
-let int_var = 1;
-let int_min = Number.MIN_SAFE_INTEGER;
+// целое число
+let int_var = 1; 
+// максимальное целое число
 let int_max = Number.MAX_SAFE_INTEGER;
+// минимальное целое число
+let int_min = Number.MIN_SAFE_INTEGER;
+
+// число с плавающей точкой
 let float_var = 3.14
+// минимальное число с плавающей точкой
 let float_min = Number.MIN_VALUE;
+// максимально число с плавающей точкой
 let float_max = Number.MAX_VALUE;
 
+// вывод типов данных и диапазоны
 console.log(`int_var: ${typeof int_var}, min: ${int_min}, max: ${int_max}`);
 console.log(`float_var: ${typeof float_var}, min: ${float_min}, max: ${float_max}`);

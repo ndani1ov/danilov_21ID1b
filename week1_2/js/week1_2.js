4eebc21e99f2fb56f23cbd8d22c716e6d8153ade
ndani1ov <132368604+ndani1ov@users.noreply.github.com>
2023-09-24 11:32:47 +0300

    добавлено задание 2

---
+++ week1_2/js/week1_2.js
@@ -0,0 +1,9 @@
+let int_var = 1;
+let int_min = Number.MIN_SAFE_INTEGER;
+let int_max = Number.MAX_SAFE_INTEGER;
+let float_var = 3.14
+let float_min = Number.MIN_VALUE;
+let float_max = Number.MAX_VALUE;
+
+console.log(`int_var: ${typeof int_var}, min: ${int_min}, max: ${int_max}`);
+console.log(`float_var: ${typeof float_var}, min: ${float_min}, max: ${float_max}`);
